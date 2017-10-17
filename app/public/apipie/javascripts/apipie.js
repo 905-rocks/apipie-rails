7303faa7b8994eb58c51f46e375ffa2a99cd970d
ysllyfe <ysllyfe@163.com>
2017-10-17 10:20:48 +0800

    semantic-template

--- app/public/apipie/javascripts/apipie.js
+++ app/public/apipie/javascripts/apipie.js
@@ -1,6 +1,291 @@
+window.semantic = {
+  handler: {}
+};
+// ready event
+semantic.ready = function() {
+  var
+    $container = $('.main.container'),
+    $sticky    = $('.ui.sticky'),
+    $document  = $(document),
+    $sectionHeaders      = $container.children('h2'),
+    $followMenu          = $container.find('.following.menu'),
+    $sectionExample      = $container.find('.example'),
+    $exampleHeaders      = $sectionExample.children('h4'),
+    $allHeaders          = $('.main.container > h2, .main.container > .tab > h2, .main.container > .tab > .examples h2'),
+    $footer              = $('.footer'),
+    $example             = $('.example'),
+    handler;
+  handler = {
+    createWaypoints: function() {
+      $sectionHeaders
+        .visibility({
+          observeChanges: false,
+          once: false,
+          offset: 50,
+          onTopPassed: handler.activate.section,
+          onTopPassedReverse: handler.activate.previous
+        })
+      ;
+
+      $sectionExample
+        .visibility({
+          observeChanges: false,
+          once: false,
+          offset: 50,
+          onTopPassed: handler.activate.example,
+          onBottomPassedReverse: handler.activate.example
+        })
+      ;
+      $footer
+        .visibility({
+          observeChanges: false,
+          once: false,
+          onBottomVisible: function(calculations) {
+            var
+              $title = $followMenu.find('> .item > .title').last()
+            ;
+            $followMenu
+              .accordion('open', $title)
+            ;
+          }
+        })
+      ;
+    },
+
+    tryCreateMenu: function(event) {
+      if($(window).width() > 640 && !$('body').hasClass('basic')) {
+        if($container.length > 0 && $container.find('.following.menu').length === 0) {
+          handler.createMenu();
+          handler.createWaypoints();
+          $(window).off('resize.menu');
+        }
+      }
+    },
+
+    createMenu: function() {
+      // grab each h3
+      var
+        html      = '',
+        pageTitle = handler.getPageTitle(),
+        title     = pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1),
+        $sticky,
+        $rail
+      ;
+      $sectionHeaders
+        .each(function(index) {
+          var
+            $currentHeader = $(this),
+            $nextElements  = $currentHeader.nextUntil('h2'),
+            $examples      = $nextElements.find('.example:not(.another)').addBack().filter('.example:not(.another)'),
+            activeClass    = (index === 0)
+              ? 'active '
+              : '',
+            text     = handler.getText($currentHeader),
+            safeName = handler.getSafeName(text),
+            id       = window.escape(safeName),
+            $anchor  = $('<a />').addClass('anchor').attr('id', id)
+          ;
+          html += '<div class="item">';
+          if($examples.length === 0) {
+            html += '<a class="'+activeClass+'title" href="#'+ id +'"><b>' + $(this).text() + '</b></a>';
+          }
+          else {
+            html += '<a class="'+activeClass+'title"><i class="dropdown icon"></i> <b>' + $(this).text() + '</b></a>';
+          }
+          if($examples.length > 0) {
+            html += '<div class="'+activeClass+'content menu">';
+            $examples
+              .each(function() {
+                var
+                  $title   = $(this).children('h4').eq(0),
+                  text     = $title.data('name'),
+                  safeName = handler.getSafeName(text),
+                  id       = window.escape(safeName),
+                  $anchor  = $('<a />').addClass('anchor').attr('id', id)
+                ;
+                if($title.length > 0) {
+                  html += '<a class="item" href="#'+id+'">' + text + '</a>';
+                }
+              })
+            ;
+            html += '</div>';
+          }
+          html += '</div>';
+        })
+      ;
+      $followMenu = $('<div />')
+        .addClass('ui vertical following fluid accordion text menu')
+        .html(html)
+      ;
+      /* Advert
+      var $advertisement = $('<div />')
+        .addClass('advertisement')
+        .html('<script type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=semanticuicom" id="_carbonads_js"></script>')
+      ;
+      */
+      $sticky = $('<div />')
+        .addClass('ui sticky')
+        .html($followMenu)
+        //.prepend($advertisement)
+        .prepend('<h4 class="ui header">' + title + '</h4>')
+      ;
+      $rail = $('<div />')
+        .addClass('ui dividing right rail')
+        .html($sticky)
+        .prependTo($container)
+      ;
+      $sticky.sticky({
+        silent: true,
+        context: $container,
+        offset: 30
+      });
+      $followMenu
+        .accordion({
+          exclusive: false,
+          animateChildren: false,
+          onChange: function() {
+            $('.ui.sticky').sticky('refresh');
+          }
+        })
+        .find('.menu a[href], .title[href]')
+          .on('click', handler.scrollTo)
+      ;
+    },
+
+    getSafeName: function(text) {
+      return text.replace(/\s+/g, '-').replace(/[^-,'A-Za-z0-9]+/g, '').toLowerCase();
+    },
+
+    getPageTitle: function() {
+      return $.trim($('h1').eq(0).contents().filter(function() { return this.nodeType == 3; }).text().toLowerCase());
+    },
+
+    getText: function($element) {
+      $element = ($element.find('a').not('.label, .anchor').length > 0)
+        ? $element.find('a')
+        : $element
+      ;
+      var
+        $text = $element.contents().filter(function(){
+          return this.nodeType == 3;
+        })
+      ;
+      return ($text.length > 0)
+        ? $text[0].nodeValue.trim()
+        : $element.find('a').text().trim()
+      ;
+    },
+
+
+    activate: {
+      previous: function() {
+        var
+          $menuItems  = $followMenu.children('.item'),
+          $section    = $menuItems.filter('.active'),
+          index       = $menuItems.index($section)
+        ;
+        if($section.prev().length > 0) {
+          $section
+            .removeClass('active')
+            .prev('.item')
+            .addClass('active')
+          ;
+          $followMenu
+            .accordion('open', index - 1)
+          ;
+        }
+      },
+      accordion: function() {
+        var
+          $section       = $(this),
+          index          = $sectionHeaders.index($section),
+          $followSection = $followMenu.children('.item'),
+          $activeSection = $followSection.eq(index)
+        ;
+      },
+      section: function() {
+        var
+          $section       = $(this),
+          index          = $sectionHeaders.index($section),
+          $followSection = $followMenu.children('.item'),
+          $activeSection = $followSection.eq(index),
+          isActive       = $activeSection.hasClass('active')
+        ;
+        if(!isActive) {
+          $followSection.filter('.active')
+            .removeClass('active')
+          ;
+          $activeSection
+            .addClass('active')
+          ;
+          $followMenu
+            .accordion('open', index)
+          ;
+        }
+      },
+      example: function() {
+        var
+          $section       = $(this).children('h4').eq(0),
+          index          = $exampleHeaders.index($section),
+          $followSection = $followMenu.find('.menu > .item'),
+          $activeSection = $followSection.eq(index),
+          inClosedTab    = ($(this).closest('.tab:not(.active)').length > 0),
+          anotherExample = ($(this).filter('.another.example').length > 0),
+          isActive       = $activeSection.hasClass('active')
+        ;
+        if(index !== -1 && !inClosedTab && !anotherExample && !isActive) {
+          $followSection.filter('.active')
+            .removeClass('active')
+          ;
+          $activeSection
+            .addClass('active')
+          ;
+        }
+      }
+    },
+
+    createAnchors: function() {
+      $allHeaders
+        .each(function() {
+          var
+            $section = $(this),
+            text     = handler.getText($section),
+            safeName = handler.getSafeName(text),
+            id       = window.escape(safeName),
+            $anchor  = $('<a />').addClass('anchor').attr('id', id)
+          ;
+          $section
+            .append($anchor)
+          ;
+        })
+      ;
+      $example
+        .each(function() {
+          var
+            $title   = $(this).children('h4').eq(0),
+            text     = handler.getText($title),
+            safeName = handler.getSafeName(text),
+            id       = window.escape(safeName),
+            $anchor  = $('<a />').addClass('anchor').attr('id', id)
+          ;
+          if($title.length > 0) {
+            $title.after($anchor);
+          }
+        })
+      ;
+    },
+  };
+
+  semantic.handler = handler;
+  handler.createAnchors();
+  handler.tryCreateMenu();
+  $(window).on('resize.menu', function() {
+    handler.tryCreateMenu();
+  });
+}
 $(document).ready(function() {
-  if (typeof prettyPrint == 'function') {
-    $('pre.ruby').addClass('prettyprint lang-rb');
-    prettyPrint();
-  }
+  $('pre.prettyprint').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
 });
+$(document).ready(semantic.ready);
